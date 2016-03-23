d51e731fceb47b0b7406fef571ab18c11f306778
MichaelLSmith <mlsmith45@gmail.com>
2016-03-23 13:46:15 -0400

    mdl vrs begin

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// Set options
+       var options = {
+           offset: '#showHere',
+           classes: {
+               clone:   'banner--clone',
+               stick:   'banner--stick',
+               unstick: 'banner--unstick'
+           }
+       };
+
+       // Initialise with options
+       var banner = new Headhesive('.banner', options);
+
+       // Headhesive destroy
+       // banner.destroy();
--- js/stickMenu.js
+++ js/stickMenu.js
@@ -1,8 +1,5 @@
 
     var stickyMenu = {
-
-        test:'this is test string',
-
         init:function(){
             console.log('init');
             this.htmlCache();
@@ -10,13 +7,15 @@
             this.insert();
             this.noStickClass();
             this.checkMenu();
+            // this.stickClass();
         },
         htmlCache:function() {
             console.log('htmlCache');
             // this.nav;
+            this.container = HTML.query('.container');
             this.nav = document.querySelector('.nav');
-            console.log('this.nav in htmlCache');
-            console.log(this.nav);
+            // console.log('this.nav in htmlCache');
+            // console.log(this.nav);
             this.body = HTML.body;
         },
         clone:function() {
@@ -27,47 +26,49 @@
         },
         insert:function() {
             console.log('insert');
-            this.body.insertBefore(this.cln, this.nav.nextSibling);
+            this.container.insertBefore(this.cln, this.nav.nextSibling);
         },
         noStickClass:function() {
             console.log('noStickClass');
             // this.htmlCache();
-            console.log(this);
-            var print = this.nav.classList;
-            console.log(print);
+            // console.log(this);
+            // var print = this.nav.classList;
+            // console.log(print);
             this.cln.classList.add('clone', 'disableMenu', 'fixedMenu');
             this.orig.classList.remove('disableMenu');
         },
         stickClass:function() {
-            console.log('stickClass');
+            // console.log('stickClass');
             //remove disableMenu from clone;
             this.cln.classList.remove('disableMenu');
             //add disableMenu to orig;
             this.orig.classList.add('disableMenu');
-            console.log(this.orig.classList);
+            // console.log(this.orig.classList);
         },
         stickMenu:function(){
-            console.log('stickMenu');
-            this.htmlCache();
-            console.log('this.orig in stickMenu');
-            console.log(this.orig);
+            // console.log('stickMenu');
+            // this.htmlCache();
+            // console.log('this.orig in stickMenu');
+            // console.log(this.orig);
+            this.clnCoord = this.cln.getBoundingClientRect();
+            // console.log(this.clnCoord);
             this.orgCoord = this.orig.getBoundingClientRect();
-            console.log(this.orgCoord.top);
+            // console.log(this.orgCoord);
 
             // if orig position === clone position --> hide orig by adding disableMenu class to orig & removing it from clone.
             if ( this.orgCoord.top <= 0 ){
-                console.log('call stickClass()');
+                // console.log('call stickClass()');
                 this.stickClass();
             }
             //do the opposite too.
             else {
-                console.log('call noStickClass');
+                // console.log('call noStickClass');
                 this.noStickClass();
             }
         },
         checkMenu:function() {
             console.log('checkMenu()');
-            this.scrollInterval = setInterval(this.stickMenu.bind(stickyMenu), 15000);
+            this.scrollInterval = setInterval(this.stickMenu.bind(stickyMenu), 200);
         }
     };
 
